1c0ae0b31783bdf045b1aa7460cd7fba98cb204d
Agutr0t <abak.93@gmail.com>
2023-06-30 10:01:52 +0200

    feat: create an error page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./routes/error-page";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />
   },
 ]);
 
