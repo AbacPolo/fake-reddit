0ec2e4783e23e529e5e45f50397c6441d616e613
Agutr0t <abak.93@gmail.com>
2023-07-04 20:06:18 +0200

    feat: separate data document

--- src/App.js
+++ src/App.js
@@ -3,66 +3,7 @@ import "./App.css";
 import { NavBar } from "./features/navBar/NavBar";
 import { NavMenu } from "./features/navMenu/NavMenu";
 import { Outlet } from "react-router-dom";
-import imagePlaceholder from "./images/test_image.jpg";
-
-const arrayOfPosts = [
-  {
-    id: 1,
-    subreddit: "maybemaybemaybe",
-    title: "Awesome",
-    imageLink: imagePlaceholder,
-    upvotes: 27,
-    commentsNumb: 57,
-    comments: [
-      {
-        profilePic: "link",
-        name: "name",
-        time: "time",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante neque, molestie et neque et, volutpat luctus ipsum. Duis nulla velit, luctus vitae accumsan in, pulvinar a risus.",
-      },
-      {
-        profilePic: "link",
-        name: "name",
-        time: "time",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante neque, molestie et neque et, volutpat luctus ipsum. Duis nulla velit, luctus vitae accumsan in, pulvinar a risus.",
-      },
-      {
-        profilePic: "link",
-        name: "name",
-        time: "time",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante neque, molestie et neque et, volutpat luctus ipsum. Duis nulla velit, luctus vitae accumsan in, pulvinar a risus.",
-      },
-      {
-        profilePic: "link",
-        name: "name",
-        time: "time",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante neque, molestie et neque et, volutpat luctus ipsum. Duis nulla velit, luctus vitae accumsan in, pulvinar a risus.",
-      },
-    ],
-  },
-  {
-    id: 2,
-    subreddit: "maybemaybemaybe",
-    title: "Awesome",
-    imageLink: imagePlaceholder,
-    upvotes: 1352,
-    commentsNumb: 850,
-    comments: [],
-  },
-  {
-    id: 3,
-    subreddit: "maybemaybemaybe",
-    title: "Awesome",
-    imageLink: imagePlaceholder,
-    upvotes: 137,
-    commentsNumb: 96,
-    comments: [],
-  },
-];
+import { arrayOfPosts } from "./data/PostInformation";
 
 function App() {
   return (
---
+++ src/data/PostInformation.js
@@ -0,0 +1,60 @@
+import imagePlaceholder from "../images/test_image.jpg";
+
+export const arrayOfPosts = [
+    {
+      id: 1,
+      subreddit: "maybemaybemaybe",
+      title: "Awesome",
+      imageLink: imagePlaceholder,
+      upvotes: 27,
+      commentsNumb: 57,
+      comments: [
+        {
+          profilePic: "link",
+          name: "name",
+          time: "time",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante neque, molestie et neque et, volutpat luctus ipsum. Duis nulla velit, luctus vitae accumsan in, pulvinar a risus.",
+        },
+        {
+          profilePic: "link",
+          name: "name",
+          time: "time",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante neque, molestie et neque et, volutpat luctus ipsum. Duis nulla velit, luctus vitae accumsan in, pulvinar a risus.",
+        },
+        {
+          profilePic: "link",
+          name: "name",
+          time: "time",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante neque, molestie et neque et, volutpat luctus ipsum. Duis nulla velit, luctus vitae accumsan in, pulvinar a risus.",
+        },
+        {
+          profilePic: "link",
+          name: "name",
+          time: "time",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante neque, molestie et neque et, volutpat luctus ipsum. Duis nulla velit, luctus vitae accumsan in, pulvinar a risus.",
+        },
+      ],
+    },
+    {
+      id: 2,
+      subreddit: "maybemaybemaybe",
+      title: "Awesome",
+      imageLink: imagePlaceholder,
+      upvotes: 1352,
+      commentsNumb: 850,
+      comments: [],
+    },
+    {
+      id: 3,
+      subreddit: "maybemaybemaybe",
+      title: "Awesome",
+      imageLink: imagePlaceholder,
+      upvotes: 137,
+      commentsNumb: 96,
+      comments: [],
+    },
+  ];
