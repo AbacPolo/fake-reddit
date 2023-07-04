270b13cca001da6d9fcb564e1e8b159a3dd27285
Agutr0t <abak.93@gmail.com>
2023-07-04 15:41:58 +0200

    feat: implement error page

--- src/error-page/error-page.js
+++ src/error-page/error-page.js
@@ -1,4 +1,5 @@
 import { useRouteError } from "react-router-dom";
+import "./error-page.css";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -6,11 +7,13 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <h2>Oops!</h2>
+      <h3>Sorry, an unexpected error has occurred.</h3>
+      <div className="ErrorMessage_Container">
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
     </div>
   );
 }
