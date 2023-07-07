34484dc87f3e1cd66eb89fdfc0a7b42f28a08283
Agutr0t <abak.93@gmail.com>
2023-07-07 10:33:43 +0200

    feat: text formater improvements

--- src/components/helpers/textPrinter.js
+++ src/components/helpers/textPrinter.js
@@ -1,40 +1,182 @@
-export const textPrinter = (string) => {
-  if (string[0] === "*" && string[1] === "*") {
+export const textPrinter = (string, index, preview) => {
+  if (
+    (string[0] === "*" && string[1] === "*") ||
+    (string[0] === " " && string[1] === "*" && string[2] === "*")
+    //checks if string starts with "** or " ** --BOLD
+  ) {
+    const boldString = string.replace(/\*/g, "");
+    if (
+      (boldString[0] === "~" && boldString[1] === "~") ||
+      (boldString[1] === "~" && boldString[2] === "~")
+      //checks if BOLD string starts with "~~ or " ~~ --CROSSED
+    ) {
+      const stringToPrint = boldString.replace(/~/g, "");
+      return (
+        <strike key={index}>
+          <strong>{stringToPrint}</strong>
+        </strike>
+      ); //return BOLD & CROSSED string
+    } else {
+      //return BOLD string
+      return (
+        <p key={index}>
+          <strong>{boldString}</strong>
+        </p>
+      );
+    }
+  } else if (
+    (string[0] === "*" && string[1] !== "*") ||
+    (string[0] === " " && string[1] === "*" && string[2] !== "*")
+    //checks if string starts with "* or " * --ITALICS
+  ) {
     const stringToPrint = string.replace(/\*/g, "");
-    return <b>{stringToPrint}</b>;
-  } else if (string[0] === "*" && string[1] !== "*") {
-    const stringToPrint = string.replace(/\*/g, "");
-    return <i>{stringToPrint}</i>;
+    return (
+      <p>
+        <em key={index}>{stringToPrint}</em>
+      </p>
+    );
   } else if (
     (string[0] === "~" && string[1] === "~") ||
-    (string[1] === "~" && string[2] === "~")
+    (string[0] === " " && string[1] === "~" && string[2] === "~")
+    //checks if string starts with "~~ or " ~~ --CROSSED
   ) {
-    const stringToPrint = string.replace(/\~/g, "");
-    return <strike>{stringToPrint}</strike>;
-  } else if (string[0] === "[") {
+    const stringToPrint = string.replace(/~/g, "");
+    return <strike key={index}>{stringToPrint}</strike>;
+  } else if (
+    (string[0] === "#" && string[1] === " ") ||
+    (string[0] === " " && string[1] === "#" && string[2] === "0")
+    //checks if string starts with "# or " # --TITLE
+  ) {
+    const stringToPrint = string.replace("# ", "");
+    return <h4 key={index}>{stringToPrint}</h4>;
+  } else if (string[0] === "[" && string.slice(-1) === ")") {
+    //checks if string starts with "[ and finishes with )" --NAME & LINK
     const prepareSeparateLink = string.replace(/\]/g, "]\r");
     const separateLink = prepareSeparateLink.split(/(?:\r)/g);
     const stringToPrint = separateLink[0].replace(/\[|\]/g, "");
     const stringLink = separateLink[1].replace(/\(|\)/g, "");
-    if (preview === "PostCard") {
-      return <p className="PostLink">{stringToPrint}</p>;
-    } else if (preview === "PostPage") {
+    if (
+      (stringToPrint[0] === "*" && stringToPrint[1] === "*") ||
+      (stringToPrint[0] === " " &&
+        stringToPrint[1] === "*" &&
+        stringToPrint[2] === "*")
+      //checks if string starts with "** or " ** --BOLD
+    ) {
+      const boldString = stringToPrint.replace(/\*/g, "");
+      if (preview === "PostCard") {
+        //print unclickable link
+        return (
+          <p key={index} className="PostLink">
+            <strong>&nbsp;{boldString}&nbsp;</strong>
+          </p>
+        );
+      } else if (preview === "PostPage") {
+        //print clickable link
+        return (
+          <a
+            key={index}
+            className="PostLink"
+            href={stringLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <strong>&nbsp;{boldString}&nbsp;</strong>
+          </a>
+        );
+      }
+    } else if (
+      (stringToPrint[0] === "*" && stringToPrint[1] !== "*") ||
+      (stringToPrint[0] === " " &&
+        stringToPrint[1] === "*" &&
+        stringToPrint[2] !== "*")
+      //checks if string starts with "* or " * --ITALICS
+    ) {
+      const italicsString = stringToPrint.replace(/\*/g, "");
+      if (preview === "PostCard") {
+        //print unclickable link
+        return (
+          <p key={index} className="PostLink">
+            <em>&nbsp;{italicsString}&nbsp;</em>
+          </p>
+        );
+      } else if (preview === "PostPage") {
+        //print clickable link
+        return (
+          <a
+            key={index}
+            className="PostLink"
+            href={stringLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <em>&nbsp;{italicsString}&nbsp;</em>
+          </a>
+        );
+      }
+    } else if (
+      (stringToPrint[0] === "~" && stringToPrint[1] === "~") ||
+      (stringToPrint[0] === " " &&
+        stringToPrint[1] === "~" &&
+        stringToPrint[2] === "~")
+      //checks if string starts with "~~ or " ~~ --CROSSED
+    ) {
+      const crossedString = stringToPrint.replace(/~/g, "");
+      if (preview === "PostCard") {
+        //print unclickable link
+        return (
+          <p key={index} className="PostLink">
+            <strike>&nbsp;{crossedString}&nbsp;</strike>
+          </p>
+        );
+      } else if (preview === "PostPage") {
+        //print clickable link
+        return (
+          <a
+            key={index}
+            className="PostLink"
+            href={stringLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <strike>&nbsp;{crossedString}&nbsp;</strike>
+          </a>
+        );
+      }
+    } else if (preview === "PostCard") {
+      //print unclickable link
       return (
-        <a className="PostLink" href={stringLink} target="_blank">
-          {stringToPrint}
+        <p key={index} className="PostLink">
+          &nbsp;{stringToPrint}&nbsp;
+        </p>
+      );
+    } else if (preview === "PostPage") {
+      //print clickable link
+      return (
+        <a
+          key={index}
+          className="PostLink"
+          href={stringLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          &nbsp;{stringToPrint}&nbsp;
         </a>
       );
     }
   } else if (string === "" || string.trim().length === 0) {
+    //checks if string is empty or all whitespace --BLANK
     return (
-      <span>
+      <span key={index}>
         <br></br>
       </span>
     );
   } else if (string === "&amp;#x200B;") {
+    //checks if string is ampersand --&
     const changeAND = string.replace("&amp;#x200B;", "&");
-    return <p>{changeAND}</p>;
-  } else {
-    return <p>{string}</p>;
+    return <p key={index}>{changeAND}</p>;
+  }
+  //default string
+  else {
+    return <p key={index}>{string}</p>;
   }
 };
--- src/components/selftextFormater/SelftextFormater.js
+++ src/components/selftextFormater/SelftextFormater.js
@@ -1,14 +1,13 @@
 import "./SelftextFormater.css";
 import classNames from "classnames";
+import { textPrinter } from "../helpers/textPrinter";
 
 export function TextFormater({ selftext, preview }) {
   const replaceBracketLeft = selftext.replace(/\[/g, "\r[");
 
   const replaceParentesiRight = replaceBracketLeft.replaceAll(") ", ")\r");
-  const splitString = replaceParentesiRight.split(/(?:\r\n|\r|\n)/g);
-
-  // console.log("splitString", splitString);
-
+  const replaceParentesiAndPointRight = replaceParentesiRight.replaceAll(").", ")\r.");
+  const splitString = replaceParentesiAndPointRight.split(/(?:\r\n|\r|\n)/g);
   const replaceWrongSpaces = splitString.map(
     (word, index) =>
       (word === " " || word === "  " || word === "   ") ? "" : word
@@ -18,63 +17,61 @@ export function TextFormater({ selftext, preview }) {
     (word, index) =>
       word !== "" || (word === "" && replaceWrongSpaces[index - 1] !== "")
   );
-
-  const textPrinter = (string, index) => {
-    if (
-      (string[0] === "*" && string[1] === "*") ||
-      (string[0] === " " && string[1] === "*" && string[2] === "*")
-    ) {
-      const stringToPrint = string.replace(/\*/g, "");
-      return <b key={index}>{stringToPrint}</b>;
-    } else if (
-      (string[0] === "*" && string[1] !== "*") ||
-      (string[0] === " " && string[1] === "*" && string[2] !== "*")
-    ) {
-      const stringToPrint = string.replace(/\*/g, "");
-      return <i key={index}>{stringToPrint}</i>;
-    } else if (
-      (string[0] === "~" && string[1] === "~") ||
-      (string[1] === "~" && string[2] === "~")
-    ) {
-      const stringToPrint = string.replace(/~/g, "");
-      return <strike key={index}>{stringToPrint}</strike>;
-    } else if (string[0] === "[" && string.slice(-1) === ")") {
-      const prepareSeparateLink = string.replace(/\]/g, "]\r");
-      const separateLink = prepareSeparateLink.split(/(?:\r)/g);
-      const stringToPrint = separateLink[0].replace(/\[|\]/g, "");
-      const stringLink = separateLink[1].replace(/\(|\)/g, "");
-      if (preview === "PostCard") {
-        return (
-          <p key={index} className="PostLink">
-            {stringToPrint}&nbsp;
-          </p>
-        );
-      } else if (preview === "PostPage") {
-        return (
-          <a
-            key={index}
-            className="PostLink"
-            href={stringLink}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {stringToPrint}&nbsp;
-          </a>
-        );
-      }
-    } else if (string === "" || string.trim().length === 0) {
-      return (
-        <span key={index}>
-          <br></br>
-        </span>
-      );
-    } else if (string === "&amp;#x200B;") {
-      const changeAND = string.replace("&amp;#x200B;", "&");
-      return <p key={index}>{changeAND}</p>;
-    } else {
-      return <p key={index}>{string}</p>;
-    }
-  };
+  //   if (
+  //     (string[0] === "*" && string[1] === "*") ||
+  //     (string[0] === " " && string[1] === "*" && string[2] === "*")
+  //   ) {
+  //     const stringToPrint = string.replace(/\*/g, "");
+  //     return <p key={index}><strong>{stringToPrint}</strong></p>;
+  //   } else if (
+  //     (string[0] === "*" && string[1] !== "*") ||
+  //     (string[0] === " " && string[1] === "*" && string[2] !== "*")
+  //   ) {
+  //     const stringToPrint = string.replace(/\*/g, "");
+  //     return <p><em key={index}>{stringToPrint}</em></p>;
+  //   } else if (
+  //     (string[0] === "~" && string[1] === "~") ||
+  //     (string[1] === "~" && string[2] === "~")
+  //   ) {
+  //     const stringToPrint = string.replace(/~/g, "");
+  //     return <strike key={index}>{stringToPrint}</strike>;
+  //   } else if (string[0] === "[" && string.slice(-1) === ")") {
+  //     const prepareSeparateLink = string.replace(/\]/g, "]\r");
+  //     const separateLink = prepareSeparateLink.split(/(?:\r)/g);
+  //     const stringToPrint = separateLink[0].replace(/\[|\]/g, "");
+  //     const stringLink = separateLink[1].replace(/\(|\)/g, "");
+  //     if (preview === "PostCard") {
+  //       return (
+  //         <p key={index} className="PostLink">
+  //           &nbsp;{stringToPrint}&nbsp;
+  //         </p>
+  //       );
+  //     } else if (preview === "PostPage") {
+  //       return (
+  //         <a
+  //           key={index}
+  //           className="PostLink"
+  //           href={stringLink}
+  //           target="_blank"
+  //           rel="noreferrer"
+  //         >
+  //           &nbsp;{stringToPrint}&nbsp;
+  //         </a>
+  //       );
+  //     }
+  //   } else if (string === "" || string.trim().length === 0) {
+  //     return (
+  //       <span key={index}>
+  //         <br></br>
+  //       </span>
+  //     );
+  //   } else if (string === "&amp;#x200B;") {
+  //     const changeAND = string.replace("&amp;#x200B;", "&");
+  //     return <p key={index}>{changeAND}</p>;
+  //   } else {
+  //     return <p key={index}>{string}</p>;
+  //   }
+  // };
 
   return (
     <div className="Selftext_Container">
@@ -84,7 +81,7 @@ export function TextFormater({ selftext, preview }) {
         })}
       >
         {eliminateRedundantSpace.map((string, index) =>
-          textPrinter(string, index)
+          textPrinter(string, index, preview)
         )}
       </div>
     </div>
