afdcc9225ca4b4744d38595a6515f86e17013773
Agutr0t <abak.93@gmail.com>
2023-07-11 10:57:44 +0200

    fix: better hash router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import "./App.css";
 import { NavBar } from "./features/navBar/NavBar";
 import { NavMenu } from "./features/navMenu/NavMenu";
-import { Outlet } from "react-router-dom";
+import { HashRouter, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="AppRoot">
-      <div className="AppWrapper">
-        <h1 className="Main_Title">FAKE REDDIT</h1>
-        <NavBar />
-        <Outlet />
-        <NavMenu />
+    <HashRouter>
+      <div className="AppRoot">
+        <div className="AppWrapper">
+          <h1 className="Main_Title">FAKE REDDIT</h1>
+          <NavBar />
+          <Outlet />
+          <NavMenu />
+        </div>
       </div>
-    </div>
+    </HashRouter>
   );
 }
 
