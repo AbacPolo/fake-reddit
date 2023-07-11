4da12198d786e2393513bae9855487da1786655c
Agutr0t <abak.93@gmail.com>
2023-07-11 11:30:19 +0200

    fix: import browserroute

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import {
+  BrowserRouter,
   HashRouter,
   RouterProvider,
   createBrowserRouter,
